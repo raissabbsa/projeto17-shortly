858b42eb1606a8a4f41198353f7635572ac69933
Raissa Barbosa <raissabbsa@gmail.com>
2022-12-22 13:15:21 -0300

    refact: connection db and port

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(authRoutes);
 
-app.listen(4000, () => {
-    console.log('Server is listening on port 4000.');
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
   });
